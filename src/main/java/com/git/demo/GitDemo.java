17d65463d4fd393a96f75313666099622f8ee1dc
saptabapi <javatech1.sapta20@gmail.com>
2022-08-29 13:07:34 +0530

    added if condition

--- src/main/java/com/git/demo/GitDemo.java
+++ src/main/java/com/git/demo/GitDemo.java
@@ -4,6 +4,9 @@ public class GitDemo {
 
 	public void showData(String msg) {
 		System.out.println("Message::"+msg);
+		if(msg.equals("user")) {
+			System.out.println("Welcome..");
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
