6c9f7d9d320517485bbeecf315474bedd4767d95
saptabapi <javatech1.sapta20@gmail.com>
2022-08-29 13:04:22 +0530

    added git sample proj

---
+++ src/main/java/com/git/demo/GitDemo.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+public class GitDemo {
+
+	public void showData(String msg) {
+		System.out.println("Message::"+msg);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
